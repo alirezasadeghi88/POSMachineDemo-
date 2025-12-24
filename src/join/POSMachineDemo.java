8a28ab3334c7ee436858b1402a67e757c1f847fa
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-24 06:53:15 -0800

    add POSMachineDemo

---
+++ src/join/POSMachineDemo.java
@@ -0,0 +1,19 @@
+package join;
+
+public class POSMachineDemo {
+    public static void main(String[] args) {
+
+        Runnable checkBalance = new PrintTask("بررسی موجودی حساب ",20);
+        Runnable sendSMS = new PrintTask("ارسال پیامک تراکنش",20);
+        Runnable logTransaction = new LogTask(40);
+
+        Thread thread1 = new Thread(checkBalance);
+        Thread thread2 = new Thread(sendSMS);
+        Thread thread3 = new Thread(logTransaction);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
