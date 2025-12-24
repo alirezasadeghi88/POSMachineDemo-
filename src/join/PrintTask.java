2384b640e414b46cc1a9d7f70f8c04e705c0595a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-24 06:47:04 -0800

    add PrintTask

---
+++ src/join/PrintTask.java
@@ -0,0 +1,18 @@
+package join;
+
+public class PrintTask implements Runnable{
+    private String message;
+    private int times;
+
+    public PrintTask(String smg,int t){
+        message = smg;
+        times = t;
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < times; i++) {
+            System.out.println(message);
+        }
+    }
+}
