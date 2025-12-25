d0e8fadf89bb3fbd8df007b36d28a6669ac0163a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-25 04:52:41 -0800

    add PosThreadControlDemo

---
+++ src/project/PosThreadControlDemo.java
@@ -0,0 +1,46 @@
+package project;
+
+
+public class PosThreadControlDemo {
+    public static void main(String[] args) {
+        System.out.println("[MAIN] POS started: beginning transaction...");
+
+        Thread cardReaderThread = new Thread(new CardReaderTask(), "CardReader");
+        Thread bankAuthThread = new Thread(new BankAuthTask(), "BankAuth");
+        Thread receiptPrinterThread = new Thread(new ReceiptPrinterTask(), "ReceiptPrinter");
+        Thread inventoryUpdateThread = new Thread(new InventoryUpdateTask(), "InventoryUpdate");
+
+        bankAuthThread.setPriority(Thread.MAX_PRIORITY);       // [BANK] حیاتی‌ترین نخ
+        receiptPrinterThread.setPriority(Thread.NORM_PRIORITY); // [PRINT] اولویت متوسط
+        inventoryUpdateThread.setPriority(Thread.MIN_PRIORITY); // [INV] کم‌اهمیت‌تر
+
+        cardReaderThread.start();
+        bankAuthThread.start();
+        receiptPrinterThread.start();
+        inventoryUpdateThread.start();
+
+
+        System.out.println("[MAIN] BankAuth alive? " + bankAuthThread.isAlive());
+        System.out.println("[MAIN] ReceiptPrinter alive? " + receiptPrinterThread.isAlive());
+
+
+        try {
+            bankAuthThread.join();
+            receiptPrinterThread.join(); // رسید باید آماده شود
+        } catch (InterruptedException e) {
+            System.out.println("[MAIN] Interrupted while waiting: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+
+
+        if (inventoryUpdateThread.isAlive()) {
+            System.out.println("[MAIN] Customer canceled transaction. Interrupting inventory update...");
+            inventoryUpdateThread.interrupt();
+        }
+
+
+        System.out.println("[MAIN] Transaction closed. POS ready for next transaction.");
+    }
+}
+
+
