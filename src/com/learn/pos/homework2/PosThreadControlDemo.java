a7034c4f7732c4d498a6bba3eccc3cda219bca89
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-27 06:43:32 -0800

    add PosThreadControlDemo

---
+++ src/com/learn/pos/homework2/PosThreadControlDemo.java
@@ -0,0 +1,39 @@
+package com.learn.pos.homework2;
+
+public class PosThreadControlDemo {
+    public static void main(String[] args) {
+        System.out.println("[MAIN] POS started: beginning transaction...");
+
+        Thread cardReaderThread = new Thread(new CardReaderTask(), "CardReader");
+        Thread bankAuthThread = new Thread(new BankAuthTask(), "BankAuth");
+        Thread receiptPrinterThread = new Thread(new ReceiptPrinterTask(), "ReceiptPrinter");
+        Thread inventoryUpdateThread = new Thread(new InventoryUpdateTask(), "InventoryUpdate");
+
+        bankAuthThread.setPriority(Thread.MAX_PRIORITY);
+        receiptPrinterThread.setPriority(Thread.NORM_PRIORITY);
+        inventoryUpdateThread.setPriority(Thread.MIN_PRIORITY);
+
+        cardReaderThread.start();
+        bankAuthThread.start();
+        receiptPrinterThread.start();
+        inventoryUpdateThread.start();
+
+        System.out.println("[MAIN] BankAuth alive? " + bankAuthThread.isAlive());
+        System.out.println("[MAIN] ReceiptPrinter alive? " + receiptPrinterThread.isAlive());
+
+        try {
+            bankAuthThread.join();
+            receiptPrinterThread.join();
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
+        if (inventoryUpdateThread.isAlive()) {
+            System.out.println("[MAIN] Customer canceled transaction. Interrupting inventory update...");
+            inventoryUpdateThread.interrupt();
+        }
+
+        System.out.println("[MAIN] Transaction closed. POS ready for next transaction.");
+
+    }
+}
