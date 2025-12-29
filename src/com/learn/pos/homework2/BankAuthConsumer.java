0197f14bcf177f3a02b441ba63a07eaff43d013c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-29 10:40:04 -0800

    add PosProducerConsumerDemo

---
+++ src/com/learn/pos/homework2/BankAuthConsumer.java
@@ -0,0 +1,18 @@
+package com.learn.pos.homework2;
+
+public class BankAuthConsumer implements Runnable {
+    private CardBuffer buffer;
+
+    public BankAuthConsumer(CardBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            String card = buffer.consume("BankAuth");
+            PosProducerConsumerDemo.sleep(800);
+            System.out.println("[BankAuth] Approved card: " + card);
+        }
+    }
+}
